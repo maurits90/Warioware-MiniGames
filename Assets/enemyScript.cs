a303e941b7d3b31ebb726735c788fda40d306832
Maurits <mauritshudaibi@gmail.com>
2021-11-23 20:48:00 +0700

    create enemy script
    - still not moving up and down

---
+++ Assets/enemyScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyScript : MonoBehaviour
+{
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime; 
+        if(transform.position.x <= -12.2)
+        {
+            transform.position = new Vector3(12.2f, transform.position.y, 0f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
+}

2c020413063c3d4ad707a2eea71bdd65939997d3
Maurits <mauritshudaibi@gmail.com>
2021-12-25 11:01:08 +0700

    Update enemyScript.cs
    add timer debug

--- Assets/enemyScript.cs
+++ Assets/enemyScript.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class enemyScript : MonoBehaviour
 {
     public float speed;
 
+    public Text asd;
 
     private float timer;
     public float maxTime = 2;
@@ -14,11 +16,13 @@ public class enemyScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        asd.text = "" + timer.ToString("F0");
         Move();
     }
 
@@ -26,22 +30,24 @@ public class enemyScript : MonoBehaviour
     void Move()
     {
         transform.position += Vector3.left * speed * Time.deltaTime;
-        
         if (transform.position.x <= -12.2f)
         {
             transform.position = new Vector3(12.2f, transform.position.y, 0f);
         }
-        
-
-
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
+        if(timer > maxTime)
         {
-            Destroy(gameObject);
+            
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+        if (timer >= 1f)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        } 
+        if (timer <= 1f)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
         }
     }
 }

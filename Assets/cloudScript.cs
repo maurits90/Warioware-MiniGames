64421b3aee1cd85084b7f9276a876323c4a3ef7e
Maurits <mauritshudaibi@gmail.com>
2022-01-04 13:36:07 +0700

    Update cloudScript.cs

--- Assets/cloudScript.cs
+++ Assets/cloudScript.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class cloudScript : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
